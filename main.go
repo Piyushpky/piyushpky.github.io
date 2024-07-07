767d848561dedfddd5eb618b81295ff6119d0f99
Piyush Kumar <piyushkumar@Piyushs-MacBook-Pro.local>
2024-07-07 17:06:33 +0530

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+package main
+
+import (
+"fmt"
+"net/http"
+"os" // Import the os package
+"strconv"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from your Go server!")
+	})
+
+	// Get port from environment, default to 8080
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		portStr = "8080" // Default port if not set
+	}
+	port, _ := strconv.Atoi(portStr)
+
+	fmt.Printf("Server listening on port %d...\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
